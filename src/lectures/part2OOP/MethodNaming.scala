dc1fac318b8f90270efd3d095ab7786a39e387d3
Przemysław Studziński <przemyslawstudzinski1993@gmail.com>
2019-03-28 21:43:23 +0100

    Method Naming Exercises

--- src/lectures/part2OOP/MethodNaming.scala
+++ src/lectures/part2OOP/MethodNaming.scala
@@ -2,12 +2,18 @@ package lectures.part2OOP
 
 object MethodNaming extends App {
 
-  class Person (val name: String, favoriteMovie: String) {
+  class Person (val name: String, favoriteMovie: String, val age : Int = 0) {
     def likes(movie: String) : Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def unary_! : String = s"$name, what the hack?!"
     def isAlive: Boolean = true
     def apply() : String = s"Hi, my name is $name and I like $favoriteMovie"
+    // Exercises
+    def +(str: String) : Person = new Person(s"$name ($str)", favoriteMovie)
+    def unary_+() : Person = new Person(name, favoriteMovie, age + 1)
+    def learns(thing : String) : String = s"$name learns $thing"
+    def learnsScala = this learns "Scala"
+    def apply(n: Int) : String = s"$name watched $favoriteMovie $n times"
   }
 
   // Infix notation = operator notation (syntactic sugar)
@@ -58,5 +64,10 @@ object MethodNaming extends App {
         mary.apply(2) => "Mary watched Inception 2 times"
  */
 
-
+  println((mary + "the Rockstar").apply())
+  println((mary + "the Rockstar")())
+  println((+mary).age)
+  println(mary learns "scala")
+  println(mary learnsScala)
+  println(mary.apply(2))
 }
