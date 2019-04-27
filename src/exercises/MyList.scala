62df62743e5b2b7ed96e6c2de307a16a4f46a053
Przemysław Studziński <przemyslawstudzinski1993@gmail.com>
2019-04-27 18:47:31 +0200

    Extend MyList to be generic

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
 
   /*
   head - first element of the list
@@ -10,35 +10,35 @@ abstract class MyList {
   to String - a string representation of list
    */
 
-  def head() : Int
-  def tail() : MyList
+  def head() : A
+  def tail() : MyList[A]
   def isEmpty() : Boolean
-  def add(n : Int) : MyList
+  def add[B >: A](n : B) : MyList[B] // "B >: A" -> super TYPE of A
   // polymorphic call
   override def toString() : String = "[" + printElements + "]"
   def printElements() : String
 }
 
-object EmptyList extends MyList {
-  override def head(): Int = throw new NoSuchElementException
+object EmptyList extends MyList[Nothing] {
+  override def head(): Nothing = throw new NoSuchElementException
 
-  override def tail(): MyList = throw new NoSuchElementException
+  override def tail(): MyList[Nothing] = throw new NoSuchElementException
 
   override def isEmpty(): Boolean = true
 
-  override def add(n: Int): MyList = new Cons(n, EmptyList)
+  override def add[B >: Nothing](n: B): MyList[B] = new Cons(n, EmptyList)
 
   override def printElements(): String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head(): Int = h
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head(): A = h
 
-  override def tail(): MyList = t
+  override def tail(): MyList[A] = t
 
   override def isEmpty(): Boolean = false
 
-  override def add(n: Int): MyList = new Cons(n, this)
+  override def add[B >: A] (n: B): MyList[B] = new Cons(n, this)
 
   override def printElements(): String = {
     if(t.isEmpty) "" + h
@@ -47,17 +47,23 @@ class Cons(h: Int, t: MyList) extends MyList {
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
-  println(list.head)
-  println(list.tail.head)
-  println(list.tail.tail.head)
-  // println(list.tail.tail.tail.head) NoSuchElementException
+// Simple (Int) list tests
+//  val list = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
+//  println(list.head)
+//  println(list.tail.head)
+//  println(list.tail.tail.head)
+//  // println(list.tail.tail.tail.head) NoSuchElementException
+//
+//  println(list.add(4).head)
+//
+//  println(list.isEmpty())
+//
+//  println(list.toString())
 
-  println(list.add(4).head)
 
-  println(list.isEmpty())
-
-
-  println(list.toString())
-
+  // Generic list tests
+  val listInts : MyList[Int] = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
+  println(listInts.printElements())
+  val stringList : MyList[String] = new Cons("Hello", new Cons("Scala", EmptyList))
+  println(stringList.printElements())
 }
